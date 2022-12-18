a11bc1e9317cddf0db398a8f766cd5090ca8144a
dddyom <dddyom@gmail.com>
2022-12-18 20:48:04 +0600

    default first launch main

--- main.py
+++ main.py
@@ -12,32 +12,37 @@ from ArgsParser import ArgsParser
 from Summary import Summary
 from utils import handle_resolved_dirs
 
-# CONFIG_PATH = '../config.ini.bak'
-CONFIG_PATH = 'config.ini'
+CONFIG_PATH = '../config.ini.bak'
+# CONFIG_PATH = 'config.ini'
+
+def main():
+    logger.info('parse config')
+    args_parser = ArgsParser(CONFIG_PATH)
+    logger.info('start detection')
+    os.system('python yolov5/detect.py ' + ' '.join(args_parser.get_args()))
+
+    logger.info('write summary')
+    Summary(
+        project_path=Path(args_parser.by_key('project')),
+        source_path=Path(args_parser.by_key('source')),
+        exp_name=args_parser.by_key('name'),
+    ).write_custom_summaries()
+
+    handle_resolved_dirs(
+        source_path=Path(args_parser.by_key('source'), )
+    )
+    logger.info('finished')
+
 
 class FileCreateHandler(FileSystemEventHandler):
     def on_created(self, event):
         if not re.search("^SO.*dat$", Path(event.src_path).name):
             return
-        logger.info('parse config')
-        args_parser = ArgsParser(CONFIG_PATH)
-        logger.info('start detection')
-        os.system('python yolov5/detect.py ' + ' '.join(args_parser.get_args()))
-
-        logger.info('write summary')
-        Summary(
-            project_path=Path(args_parser.by_key('project')),
-            source_path=Path(args_parser.by_key('source')),
-            exp_name=args_parser.by_key('name'),
-        ).write_custom_summaries()
-
-        handle_resolved_dirs(
-            source_path=Path(args_parser.by_key('source'), )
-        )
-        logger.info('finished')
+        main()
 
 
 if __name__ == '__main__':
+    main()
     logger.info('run file listener')
     event_handler = FileCreateHandler()
     observer = Observer()
